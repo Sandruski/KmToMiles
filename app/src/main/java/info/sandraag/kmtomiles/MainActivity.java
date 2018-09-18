16186cf3e2b46dc53105aa80426b7cd25d4d5f71
sandraag <sandruskiag@gmail.com>
2018-09-18 13:38:54 +0200

    Added AboutActivity

--- app/src/main/java/info/sandraag/kmtomiles/MainActivity.java
+++ app/src/main/java/info/sandraag/kmtomiles/MainActivity.java
@@ -1,5 +1,6 @@
 package info.sandraag.kmtomiles;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -44,5 +45,10 @@ public class MainActivity extends AppCompatActivity {
             String skm = Float.toString(km);
             edit_km.setText(skm);
         }
+    }
+
+    public void onClickAbout(View view) {
+        Intent intent = new Intent(this, AboutActivity.class);
+        startActivity(intent);
     }
 }
